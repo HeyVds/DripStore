26d45ec28658326e6b9614f1c638d9d1d6e78809
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-24 19:34:01 -0300

    implementação do framework para slide

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,3 +1,30 @@
+import './slide.css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+
 export const Slide = () => {
-    return ( <></> );
+    const data = 
+    [
+    
+    {id: '2' , image: './src/assets/White-Sneakers-PNG-Clipart 1.png'},
+    {id: '1' , image: './src/assets/White-Sneakers-PNG-Clipart 1.png'},
+    {id: '3' , image: './src/assets/White-Sneakers-PNG-Clipart 1.png'},
+    {id: '4' , image: './src/assets/White-Sneakers-PNG-Clipart 1.png'}
+
+    ]
+
+
+    return ( <>
+    
+        <Swiper>
+
+        <SwiperSlide>
+        
+
+        </SwiperSlide>
+
+        </Swiper>
+    
+    
+    </> );
 }
