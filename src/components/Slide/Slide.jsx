01fbd6163254b9bf3ac3df893fe25e5749dfd133
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-24 19:51:52 -0300

    mapeamento de imagens ok

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -6,8 +6,8 @@ export const Slide = () => {
     const data = 
     [
     
-    {id: '2' , image: './src/assets/White-Sneakers-PNG-Clipart 1.png'},
     {id: '1' , image: './src/assets/White-Sneakers-PNG-Clipart 1.png'},
+    {id: '2' , image: './src/assets/White-Sneakers-PNG-Clipart 1.png'},
     {id: '3' , image: './src/assets/White-Sneakers-PNG-Clipart 1.png'},
     {id: '4' , image: './src/assets/White-Sneakers-PNG-Clipart 1.png'}
 
@@ -18,10 +18,41 @@ export const Slide = () => {
     
         <Swiper>
 
-        <SwiperSlide>
-        
+        {data.map((item) => (
+
+        <SwiperSlide key={item.id}>
+            <div className='slide'>
+                <div className='Text'>
+                    <p className='p-acima'>
+                        Melhores ofertas personalizadas
+                    </p>
+
+                    <h1 className='Text-h1'>
+                    Queima de stoque Nike 🔥
+                    </h1>
+
+                    <p className='p-abaixo'>
+                    Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.
+                    </p>
+
+                </div>
+
+                <div className='div-img'>
+                    <div className='Ornament'>
+                        <img src="./src/assets/Ornament 11.png" alt="Ornament" />
+                        
+                    </div>
+
+                    <img src={item.image} alt="White-Sneakers" 
+                    className='Slide-item'
+                    />
+                </div>
+            </div>
 
         </SwiperSlide>
+
+        ))}
+
 
         </Swiper>
     
