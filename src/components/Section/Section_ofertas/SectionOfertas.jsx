ab041d4f2926c32097f9eb00f3e0486370e94a32
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-26 15:14:51 -0300

    estrutura de Section_ofertas ok

---
+++ src/components/Section/Section_ofertas/SectionOfertas.jsx
@@ -0,0 +1,23 @@
+import './section_ofertas'
+
+
+
+export const SectionOfertas = () => {
+    return ( 
+        <>
+        <div className='section-ofertas'>
+            <div className='texto-ofertas'>
+                <p className='p-ofertas-acima'></p>
+                <h2 className='h2-ofertas'></h2>
+                <p className='p-ofertas-abaixo'></p>
+            </div>
+
+            <div className='img-ofertas'>
+                <img src="" alt="" />
+            </div>
+        </div>
+        
+        
+        </>
+     );
+}
