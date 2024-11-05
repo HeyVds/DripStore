3e5dfb3d8ce3e8d0cd8d1723b04a14a651845908
vingenesi <vinnyvvv04@gmail.com>
2024-11-05 19:42:06 -0300

    Incluindo rota de ProductListing e ProductView

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { Header } from "./components/Header/Header";
 import { Error } from "./components/error/Error";
+import { ProductListingPage } from "./pages/ProductListingPage";
+import { ProductViewPage } from "./pages/ProductViewPage";
 
 export const RoutesApp = () => {
   return (
@@ -10,6 +12,8 @@ export const RoutesApp = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Error />} />
+        <Route path="/productlisting" element={<ProductListingPage />} />
+        <Route path="/productview" element={<ProductViewPage />} />
       </Routes>
       {/* <Footer /> */}
     </BrowserRouter>
