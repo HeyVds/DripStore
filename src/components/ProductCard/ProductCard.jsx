1c13faea85f8e524e7b480447248401a20d1e153
vingenesi <vinnyvvv04@gmail.com>
2024-11-05 21:24:41 -0300

    Reformulação dos estilos, trocando de tailwind para CSS

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,65 +1,30 @@
+import './productcard.css';
 import tenisImg from "../../assets/slide-image/product.png";
 
 export const ProductCard = (props) => {
   return (
-    <div className={`flex flex-col items-center mb-5 cursor-pointer`}
-    >
-      <div
-        className={`
-            w-[350px]
-            h-[439px]
-            bg-white 
-            rounded 
-            shadow-md   
-            flex
-            justify-center 
-            items-center
-            relative
-        `}
-      >
-        {props.discountPercentual && (
-          <span
-            className="
-                absolute 
-                top-2
-                left-2
-                px-3 
-                py-2 
-                max-w-44
-                rounded-full 
-                bg-[#E7FF86]
-                text-xs
-                font-bold
-                text-[#474747]
-            "
-          >
-            {props.discountPercentual}% OFF
-          </span>
+    <div className="product-card">
+      <div lassName="product-card-image">
+      {props.discountPercentual && (
+      <span className="discount-badge">
+      {props.discountPercentual}% OFF
+      </span>
         )}
-        <div className="flex items-center justify-center relative -top-3 -left-3">
-        <img className="" src={tenisImg} alt="" />
+        <div className="product-card-img-wrapper">
+        <img className="" src={tenisImg} alt="Produto" />
         </div>
       </div>
-      <div className="flex flex-col gap-1 pt-3">
-        <span className="text-[#8F8F8F] font-bold text-[12px] w-9 h-6">
+      <div className="product-card-info">
+        <span className="category">
           {props.category}
         </span>
-        <p className="text-[#474747] text-2xl">{props.productName}</p>
-        <div className="flex gap-2 items-center">
-          <span
-            className={`
-            text-2xl font-bold
-            ${props.isDiscount && "text-[#8F8F8F] line-through"}
-        `}
-          >
+        <p className="product-name">{props.productName}</p>
+        <div className="price-info">
+        <span className={`price ${props.isDiscount ? "discount" : ""}`}>
             {props.price}
           </span>
-          {props.isDiscount ? (
-            <span className="text-[#1F1F1F] text-2xl font-bold">
-              {props.discountPrice}
-            </span>
-          ) : (
-            ""
+          {props.isDiscount && (
+            <span className="discount-price">{props.discountPrice}</span>
           )}
         </div>
       </div>
--- src/components/productListing/ProductListing.jsx
+++ src/components/productListing/ProductListing.jsx
@@ -1,32 +1,31 @@
 import { ProductCard } from '../ProductCard/ProductCard';
 import { cardsInfo } from '../../utils/cardsMock';
 import { Subtitles } from '../Subtitles/Subtitles';
+import './productlisting.css';
 
 export const ProductListing = () => {
-    return ( 
-        <main className="p-7 bg-[#F9F8FE] sm:p-20">
-      <div className="flex items-center justify-between">
+  return ( 
+    <main className="product-listing">
+      <div className="header">
         <Subtitles type="secondary">Produtos em alta</Subtitles>
-        <a href="/produtos" className="text-[#C92071]">
+        <a href="/produtos" className="view-all-link">
           Ver todos ➝
         </a>
       </div>
-      <div className="w-full grid gap-1 grid-cols-1 sm:grid-cols-1 lg:grid-cols-4">
-        {cardsInfo.map((card, index) => {
-          return (
-            <ProductCard
-              key={card.id}
-              discountPercentual={index < 2 ? card.discountPercentual : null}
-              category={card.category}
-              productName={card.productName}
-              discountPrice={card.discountPrice}
-              price={card.price}
-              isDiscount={card.isDiscount}
-              url={card.url}
-            />
-          );
-        })}
+      <div className="product-grid">
+        {cardsInfo.map((card, index) => (
+          <ProductCard
+            key={card.id}
+            discountPercentual={index < 2 ? card.discountPercentual : null}
+            category={card.category}
+            productName={card.productName}
+            discountPrice={card.discountPrice}
+            price={card.price}
+            isDiscount={card.isDiscount}
+            url={card.url}
+          />
+        ))}
       </div>
     </main>
-     );
+  );
 };
