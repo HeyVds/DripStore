5be649a7c1f3a509593350159475d7baf72737b8
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-11-12 13:58:39 -0300

    teste

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 // import { ProductListing } from "../components/productListing/ProductListing"
-// import { SectionOfertas } from "../components/Section/Section_ofertas/SectionOfertas"
+import { SectionOfertas } from "../components/Section/Section_ofertas/SectionOfertas"
 // import { Slide } from "../components/Slide/Slide"
 // import {SectionCards} from "../components/Section/SectionCards/SectionCards";
 // import {Section3} from "../components/Section/Section3/Section3"
@@ -10,12 +10,12 @@ import { SlideTenis } from "../components/SlideTenis/SlideTenis"
 export const HomePage = () => {
     return (
         <>
-        {/* <Slide></Slide>
-         <SectionCards></SectionCards>
-         <Section3></Section3>
-        <SectionOfertas></SectionOfertas> */}
+        {/* <Slide></Slide> */}
+         {/* <SectionCards></SectionCards> */}
+         {/* <Section3></Section3> */}
+        <SectionOfertas></SectionOfertas>
         
-            <SlideTenis></SlideTenis>
+            {/* <SlideTenis></SlideTenis> */}
         </>
     )
 }
