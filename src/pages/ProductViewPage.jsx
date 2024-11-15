0833deb569e867582c6fab9b18d5518d00a14d76
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-11-15 13:28:27 -0300

    slice de produtos e mini header

--- src/pages/ProductViewPage.jsx
+++ src/pages/ProductViewPage.jsx
@@ -1,10 +1,41 @@
 import { SlideTenis } from "../components/SlideTenis/SlideTenis";
+import { ProductCard } from "../components/ProductCard/ProductCard"
+import { cardsInfo } from "../utils/cardsMock";
+
+
 
 export const ProductViewPage = () => {
   return (
     <>
-      <section>
-        <SlideTenis></SlideTenis>
+      <section className="viwePage">
+        <div className="mini-nav">
+          <p className="home-mini">Home</p>
+          <p>/ Produtos / Tênis / Nike / Tênis Nike Revolution 6 Next Nature Masculino</p>
+        </div>
+        <SlideTenis />
+
+        <div className="product-listingg">
+      <div className="header">
+        <div className="text-prod">Produtos Relacionados</div>
+        <a href="/produtos" className="view-all-link">
+          Ver todos ➝
+        </a>
+      </div>
+      <div className="product-grid">
+        {cardsInfo.slice(0, 4).map((card, index) => (
+          <ProductCard
+            key={card.id}
+            discountPercentual={index < 2 ? card.discountPercentual : null}
+            category={card.category}
+            productName={card.productName}
+            discountPrice={card.discountPrice}
+            price={card.price}
+            isDiscount={card.isDiscount}
+            url={card.url}
+          />
+        ))}
+      </div>
+    </div>
       </section>
     </>
   );
