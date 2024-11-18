a9770c7b2850ef091f94926cb5d1ab9fa0e929af
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-11-18 11:21:46 -0300

    criando formulario de registro credencial

---
+++ src/components/CriarContaForm/CriarConta.jsx
@@ -0,0 +1,31 @@
+import "./criarConta.css"
+
+export const CriarConta = () => {
+    return ( 
+        
+        
+        <section className="section-content-form">
+            <div className="title-form-criar">
+                <h1>Criar conta</h1>
+            </div>
+                <div className="contentainer-form-pessoal">
+
+                    <div className="content-form-pessoal">
+                        <div className="title-line">
+                            <div className="title-form-pessoal"><h3>informações pessoais</h3></div>
+                            
+                            <div className="line"></div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="contentainer-form-entrega">
+
+                </div>
+
+        </section>
+            
+
+     );
+}
