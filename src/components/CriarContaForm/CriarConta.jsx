86e99072bb68a42d649f08cf46c9a7fc85a31f91
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-11-18 14:17:11 -0300

    form de criação de conta do usuário

--- src/components/CriarContaForm/CriarConta.jsx
+++ src/components/CriarContaForm/CriarConta.jsx
@@ -1,4 +1,7 @@
 import "./criarConta.css"
+import { FormInput } from "../inputs/FormInput";
+import { Button } from "../Button/Button";
+
 
 export const CriarConta = () => {
     return ( 
@@ -12,18 +15,74 @@ export const CriarConta = () => {
 
                     <div className="content-form-pessoal">
                         <div className="title-line">
-                            <div className="title-form-pessoal"><h3>informações pessoais</h3></div>
+                            <h3>Informações Pessoais</h3>
                             
-                            <div className="line"></div>
+                            <hr className="line" />
                         </div>
+
+                        
+            <FormInput
+                title="Nome completo *"
+                placeholder="Insira seu nome"
+            />
+              <FormInput
+                title="CPF *"
+                placeholder="Insira seu CPF"
+            />
+              <FormInput
+                title="E-mail *"
+                placeholder="Insira seu email"
+            />
+              <FormInput
+                title="Celular *"
+                placeholder="Insira seu celular"
+            />
 
                     </div>
                 </div>
 
                 <div className="contentainer-form-entrega">
 
+                <div className="content-form-entrega">
+                        <div className="title-line">
+                            <h3>Informações de Entrega</h3>
+                            
+                            <hr className="line" />
+                        </div>
+
+                        
+            <FormInput
+                title="Endereço *"
+                placeholder="Insira seu endereço"
+            />
+              <FormInput
+                title="Bairro *"
+                placeholder="Insira seu bairro"
+            />
+              <FormInput
+                title="Cidade *"
+                placeholder="Insira sua cidade"
+            />
+              <FormInput
+                title="CEP *"
+                placeholder="Insira seu CEP"
+            />
+             <FormInput
+                title="Complemento *"
+                placeholder="Insira complemento"
+            />
+                    </div>
                 </div>
 
+            <div className="termos">
+                <input type="checkbox" />
+                <p>Quero receber por email ofertas e novidades das lojas da Digital Store. A frequência de envios pode variar de acordo com a interação do cliente.</p>
+
+            </div>
+
+                <div className="button-criar">
+                    <Button label="Criar conta" size="x-large" style="primary" />
+                    </div>
         </section>
             
 
