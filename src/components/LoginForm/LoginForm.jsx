e619e25e01beac4778d07970bec470c9c04d779a
vingenesi <vinnyvvv04@gmail.com>
2024-11-22 00:57:08 -0300

    Sistema de Login com token e integração na aba user

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,90 +1,97 @@
 import "./loginform.css";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { api } from "../../../service/instance";
-
+import { useEffect, useState } from "react";
 
 export const LoginForm = () => {
-
   const [hasToken, setHasToken] = React.useState(localStorage.getItem("authToken"));
-
-
   const [form, setForm] = React.useState({
-    email:"",
-    password:"",
-  })
+    email: "",
+    password: "",
+  });
+  
+  const navigate = useNavigate();
 
-
-  async function handleSubmit(e){
+  async function handleSubmit(e) {
     e.preventDefault();
-    const { data } = await api.post("/users/login", form);
-    localStorage.setItem("authToken", data.authToken);
-    setHasToken(data.authToken)
+    try {
+      const { data } = await api.post("/users/login", form);
+      
+      // Armazenar o token no localStorage
+      localStorage.setItem("authToken", data.authToken);
+      setHasToken(data.authToken);
+
+      // Redirecionar para a página "home"
+      navigate("/");
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      alert("Login falhou. Verifique suas credenciais.");
+    }
   }
 
-    function handleChange({target}) {
-      const { id, value} = target;
-
-      setForm (
-        {
-          ...form,
-          [id]: value,
-        
-        }
-      )
-        
-    }
-
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({
+      ...form,
+      [id]: value,
+    });
+  }
 
   return (
-    <>
-      <div className="login-container">
-        <div className="login-form">
-          <h2>Acesse sua conta</h2>
-          <p>
-            Novo cliente? Então registre-se <a href="register">aqui</a>.
-          </p>
-          <form onSubmit={handleSubmit}>
-            <label>Login *</label>
-            <input
-              type="email"
-              placeholder="Insira seu login ou email"
-              required onChange={handleChange}
-              id="email"
-            />
+    <div className="login-container">
+      <div className="login-form">
+        <h2>Acesse sua conta</h2>
+        <p>
+          Novo cliente? Então registre-se <a href="register">aqui</a>.
+        </p>
+        <form onSubmit={handleSubmit}>
+          <label>Login *</label>
+          <input
+            type="email"
+            placeholder="Insira seu login ou email"
+            required
+            onChange={handleChange}
+            id="email"
+          />
 
-            <label>Senha *</label>
-            <input type="password" placeholder="Insira sua senha" required  onChange={handleChange}
-            id="password"/>
+          <label>Senha *</label>
+          <input
+            type="password"
+            placeholder="Insira sua senha"
+            required
+            onChange={handleChange}
+            id="password"
+          />
 
-            <a href="#" className="forgot-password">
-              Esqueci minha senha
-            </a>
+          <a href="#" className="forgot-password">
+            Esqueci minha senha
+          </a>
 
-            <button type="submit" className="login-button">
-              Acessar Conta
-            </button>
-          </form>
-          <div className="social-login">
-            <p>Ou faça login com</p>
-            <div className="social-icons">
-              <img src="src/assets/login-image/gmail.png" alt="Gmail" />
-              <img src="src/assets/login-image/facebook.png" alt="Facebook" />
-            </div>
+          <button type="submit" className="login-button">
+            Acessar Conta
+          </button>
+        </form>
+        <div className="social-login">
+          <p>Ou faça login com</p>
+          <div className="social-icons">
+            <img src="src/assets/login-image/gmail.png" alt="Gmail" />
+            <img src="src/assets/login-image/facebook.png" alt="Facebook" />
           </div>
         </div>
-        <div className="login-image">
-          <img
-            className="tenisL"
-            src="src/assets/login-image/tenisL.png"
-            alt="Shoe 1"
-          />
-          <img
-            className="tenisR"
-            src="src/assets/login-image/tenisR.png"
-            alt="Shoe 1"
-          />
-        </div>
       </div>
-    </>
+      <div className="login-image">
+        <img
+          className="tenisL"
+          src="src/assets/login-image/tenisL.png"
+          alt="Shoe 1"
+        />
+        <img
+          className="tenisR"
+          src="src/assets/login-image/tenisR.png"
+          alt="Shoe 2"
+        />
+      </div>
+    </div>
   );
 };
--- src/components/User/MinhasInformacoes/MinhasInformacoes.jsx
+++ src/components/User/MinhasInformacoes/MinhasInformacoes.jsx
@@ -1,24 +1,31 @@
-import React, { useState,  useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./minhasInformacoes.css";
-import { api } from "../../../../service/instance"
-
-
-
-
+import { api } from "../../../../service/instance";
+import { jwtDecode } from "jwt-decode";
 
 export const MinhasInformacoes = () => {
-
-
   const [latestUser, setLatestUser] = useState(null);
 
   useEffect(() => {
     const fetchLatestUser = async () => {
       try {
-        const { data } = await api.get("https://ds-api-5owi.onrender.com/userwithaddress/10");
+        // Obtém o token do localStorage
+        const token = localStorage.getItem("authToken");
+        
+        const decodedToken = jwtDecode(token);
+        const userId = decodedToken.id;
+
+        // Faz a requisição à API com o token no cabeçalho
+        const { data } = await api.get(`https://ds-api-5owi.onrender.com/userwithaddress/${userId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        
         setLatestUser(data);
-        console.log(data)
+        console.log(data);
       } catch (error) {
-        console.error(error);
+        console.error("Erro ao buscar as informações do usuário:", error);
       }
     };
 
@@ -29,58 +36,48 @@ export const MinhasInformacoes = () => {
     return <p>Carregando...</p>;
   }
 
-
   return (
-    <>
-      <div className="container-my-infos">
-        <section className="title-my-infos">
-          <h3>Minhas Informações</h3>
-          <a href="#">Editar</a>
-        </section>
+    <div className="container-my-infos">
+      <section className="title-my-infos">
+        <h3>Minhas Informações</h3>
+        <a href="#">Editar</a>
+      </section>
 
-        <hr className="line-my-infos" />
+      <hr className="line-my-infos" />
 
-        <section className="infos-pessoais">
-          <h4 className="subtitles-my-infos">Informações pessoais</h4>
-          <p className="text-my-infos">
-            <span className="label-my-infos">Nome: {latestUser.name || "Não informado"}</span>
-            
-          </p>
-          <p className="text-my-infos">
-            <span className="label-my-infos">CPF: {latestUser.cpf || "Não informado"}</span>
-            
-            
-          </p>
-          <p className="text-my-infos">
-            <span className="label-my-infos">E-mail: {latestUser.email || "Não informado"}</span>
-          </p>
-          <p className="text-my-infos">
-            <span className="label-my-infos">Telefone: {latestUser.fone || "Não informado"}</span>
-          </p>
-        </section>
+      <section className="infos-pessoais">
+        <h4 className="subtitles-my-infos">Informações pessoais</h4>
+        <p className="text-my-infos">
+          <span className="label-my-infos">Nome: {latestUser.name || "Não informado"}</span>
+        </p>
+        <p className="text-my-infos">
+          <span className="label-my-infos">CPF: {latestUser.cpf || "Não informado"}</span>
+        </p>
+        <p className="text-my-infos">
+          <span className="label-my-infos">E-mail: {latestUser.email || "Não informado"}</span>
+        </p>
+        <p className="text-my-infos">
+          <span className="label-my-infos">Telefone: {latestUser.fone || "Não informado"}</span>
+        </p>
+      </section>
 
-        <hr className="line-my-infos" />
+      <hr className="line-my-infos" />
 
-        <section>
-          <h4 className="subtitles-my-infos">Informações de entrega</h4>
-          <p className="text-my-infos">
-            <span className="label-my-infos">Logradouro: {latestUser.rua || "Não informado"}</span>
-          </p>
-          <p className="text-my-infos">
-            <span className="label-my-infos">Bairro: {latestUser.bairro || "Não informado"}</span>
-            
-          </p>
-          
-          <p className="text-my-infos">
-            <span className="label-my-infos">Cidade: {latestUser.cidade || "Não informado"}</span>
-          </p>
-          <p className="text-my-infos">
-            <span className="label-my-infos">Estado: </span>Ceará
-          </p>
-        </section>
-        
-      </div>
-       
-    </>
+      <section>
+        <h4 className="subtitles-my-infos">Informações de entrega</h4>
+        <p className="text-my-infos">
+          <span className="label-my-infos">Logradouro: {latestUser.rua || "Não informado"}</span>
+        </p>
+        <p className="text-my-infos">
+          <span className="label-my-infos">Bairro: {latestUser.bairro || "Não informado"}</span>
+        </p>
+        <p className="text-my-infos">
+          <span className="label-my-infos">Cidade: {latestUser.cidade || "Não informado"}</span>
+        </p>
+        <p className="text-my-infos">
+          <span className="label-my-infos">Estado: </span>Ceará
+        </p>
+      </section>
+    </div>
   );
 };
