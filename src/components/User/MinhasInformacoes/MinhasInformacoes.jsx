f80452b7d699fe07731408bd3cad4e88580fc9dd
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-11-21 23:26:49 -0300

    token

--- src/components/User/MinhasInformacoes/MinhasInformacoes.jsx
+++ src/components/User/MinhasInformacoes/MinhasInformacoes.jsx
@@ -1,6 +1,35 @@
+import React, { useState,  useEffect } from "react";
 import "./minhasInformacoes.css";
+import { api } from "../../../../service/instance"
+
+
+
+
 
 export const MinhasInformacoes = () => {
+
+
+  const [latestUser, setLatestUser] = useState(null);
+
+  useEffect(() => {
+    const fetchLatestUser = async () => {
+      try {
+        const { data } = await api.get("https://ds-api-5owi.onrender.com/userwithaddress/10");
+        setLatestUser(data);
+        console.log(data)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchLatestUser();
+  }, []);
+
+  if (!latestUser) {
+    return <p>Carregando...</p>;
+  }
+
+
   return (
     <>
       <div className="container-my-infos">
@@ -14,17 +43,19 @@ export const MinhasInformacoes = () => {
         <section className="infos-pessoais">
           <h4 className="subtitles-my-infos">Informações pessoais</h4>
           <p className="text-my-infos">
-            <span className="label-my-infos">Nome: </span>Lorem ipsum dolor sit
-            amet
+            <span className="label-my-infos">Nome: {latestUser.name || "Não informado"}</span>
+            
           </p>
           <p className="text-my-infos">
-            <span className="label-my-infos">CPF: </span>123.456.789-01
+            <span className="label-my-infos">CPF: {latestUser.cpf || "Não informado"}</span>
+            
+            
           </p>
           <p className="text-my-infos">
-            <span className="label-my-infos">E-mail: </span>email@email.com
+            <span className="label-my-infos">E-mail: {latestUser.email || "Não informado"}</span>
           </p>
           <p className="text-my-infos">
-            <span className="label-my-infos">Telefone: </span>(85)99999-9999
+            <span className="label-my-infos">Telefone: {latestUser.fone || "Não informado"}</span>
           </p>
         </section>
 
@@ -33,21 +64,23 @@ export const MinhasInformacoes = () => {
         <section>
           <h4 className="subtitles-my-infos">Informações de entrega</h4>
           <p className="text-my-infos">
-            <span className="label-my-infos">Logradouro: </span>Rua Washington
-            Soares, 123
+            <span className="label-my-infos">Logradouro: {latestUser.rua || "Não informado"}</span>
           </p>
           <p className="text-my-infos">
-            <span className="label-my-infos">Bairro: </span>Cidade dos
-            Funcionários
+            <span className="label-my-infos">Bairro: {latestUser.bairro || "Não informado"}</span>
+            
           </p>
+          
           <p className="text-my-infos">
-            <span className="label-my-infos">Cidade: </span>Fortaleza
+            <span className="label-my-infos">Cidade: {latestUser.cidade || "Não informado"}</span>
           </p>
           <p className="text-my-infos">
             <span className="label-my-infos">Estado: </span>Ceará
           </p>
         </section>
+        
       </div>
+       
     </>
   );
 };
